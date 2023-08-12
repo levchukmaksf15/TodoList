82c80680d5e031d0866027eb9b39f36ba35130c9
Levchuk Maks <levchukmaksf@gmail.com>
2023-08-12 18:41:04 +0300

    Task controller create, create-task.html and update-task.html created

--- src/main/java/com/softserve/itacademy/controller/TaskController.java
+++ src/main/java/com/softserve/itacademy/controller/TaskController.java
@@ -1,41 +1,73 @@
 package com.softserve.itacademy.controller;
 
+import com.softserve.itacademy.model.Task;
+import com.softserve.itacademy.service.StateService;
+import com.softserve.itacademy.service.TaskService;
+import com.softserve.itacademy.service.ToDoService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/tasks")
 public class TaskController {
 
-    //add needed fields
+    TaskService taskService;
+    ToDoService toDoService;
+    StateService stateService;
 
-//    @GetMapping("/create/todos/{todo_id}")
-//    public String create(//add needed parameters) {
-//        //ToDo
-//        return " ";
-//    }
-//
-//    @PostMapping("/create/todos/{todo_id}")
-//    public String create(//add needed parameters) {
-//        //ToDo
-//        return " ";
-//    }
-//
-//    @GetMapping("/{task_id}/update/todos/{todo_id}")
-//    public String update(//add needed parameters) {
-//        //ToDo
-//        return " ";
-//    }
-//
-//    @PostMapping("/{task_id}/update/todos/{todo_id}")
-//    public String update(//add needed parameters) {
-//       //ToDo
-//        return " ";
-//    }
-//
-//    @GetMapping("/{task_id}/delete/todos/{todo_id}")
-//    public String delete(//add needed parameters) {
-//        //ToDo
-//        return " ";
-//    }
+    @Autowired
+    public TaskController(TaskService taskService, ToDoService toDoService, StateService stateService) {
+        this.taskService = taskService;
+        this.toDoService = toDoService;
+        this.stateService = stateService;
+    }
+
+    @GetMapping("/create/todos/{todo_id}")
+    public String saveTask(@PathVariable Integer todo_id, Model model) {
+        Task task = new Task();
+        task.setTodo(toDoService.readById(todo_id));
+
+        model.addAttribute("task", task);
+        model.addAttribute("todo_id", todo_id);
+
+        return "create-task";
+    }
+
+    @PostMapping("/create/todos/{todo_id}")
+    public String createTask(@ModelAttribute(name="task") Task task, @PathVariable Integer todo_id,  Model model) {
+        task.setTodo(toDoService.readById(todo_id));
+        taskService.create(task);
+        return "redirect:/todos/" + todo_id + "/tasks";
+    }
+
+    @GetMapping("/{task_id}/update/todos/{todo_id}")
+    public String updateTask(@PathVariable Integer task_id, @PathVariable Integer todo_id, Model model) {
+        Task task = taskService.readById(task_id);
+        task.setTodo(toDoService.readById(todo_id));
+
+        model.addAttribute("task", task);
+        model.addAttribute("todo_id", todo_id);
+        model.addAttribute("task_id", task_id);
+
+        return "update-task";
+    }
+
+    @PostMapping("/{task_id}/update/todos/{todo_id}")
+    public String saveUpdatedTask(@ModelAttribute(name="task") Task task,
+                                  @PathVariable Integer todo_id) {
+        task.setState(stateService.getByName(task.getState().getName()));
+        task.setTodo(toDoService.readById(todo_id));
+        taskService.update(task);
+        System.out.println(task);
+        return "redirect:/todos/" + todo_id + "/tasks";
+    }
+
+    @GetMapping("/{task_id}/delete/todos/{todo_id}")
+    public String delete(@PathVariable Integer task_id,
+                         @PathVariable Integer todo_id) {
+        taskService.delete(task_id);
+        return "redirect:/todos/" + todo_id + "/tasks";
+    }
 }
